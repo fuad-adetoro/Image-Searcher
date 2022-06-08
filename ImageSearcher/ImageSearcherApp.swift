78055b1de263c8d25ebf6b384294bbe7bd883588
Fuad Adetoro <fuad@inhaus.ai>
2022-06-08 17:35:35 +0100

    Initial Commit

---
+++ ImageSearcher/ImageSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearcherApp.swift
+//  ImageSearcher
+//
+//  Created by Fuad on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
